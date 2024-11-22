03f7f5433bb9772374d4d0a4c54260993e0e9166
Gil Broochian <gil@broochian.com>
2024-11-21 21:00:00 -0500

    Use packagesFromDirectoryRecursive

--- default.nix
+++ default.nix
@@ -1,1 +1,11 @@
-import ./packages
+{ pkgs }:
+
+let
+  internal = pkgs.lib.callPackagesWith pkgs ./internal { };
+  callPackage = pkgs.lib.callPackageWith (pkgs // internal // packages);
+  packages = pkgs.lib.packagesFromDirectoryRecursive {
+    inherit callPackage;
+    directory = ./packages;
+  };
+in
+  packages
